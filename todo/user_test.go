a6010d53418feff0082a2dc7c7148ded438220e8
Spatulox <M78stormtrooper@laposte.net>
2024-10-25 16:00:37 +0200

    update user_test.go

--- todo/user_test.go
+++ todo/user_test.go
@@ -7,31 +7,40 @@ import (
 )
 
 func TestUser(t *testing.T) {
-	
 	_, err := NewUser("welp.welp@gmail.com", "prenom", "nom", "passworD12", time.Now().AddDate(-20, 0, 0))
 	assert.Nil(t, err, err)
+}
 
-	// Wrong email
-	_, err = NewUser("@gmail.com", "welp", "welp", "coucoU12", time.Now().AddDate(-20, 0, 0))
+func TestAge(t *testing.T) {
+	// Wrong Time
+	_, err := NewUser("welp.welp@gmail.com", "welp", "welp", "coucoU12", time.Now().AddDate(20, 0, 0))
 	assert.NotNil(t, err, err)
-	// Wrong Name
-	_, err = NewUser("welp.welp@gmail.com", "welp", "", "coucoU12", time.Now().AddDate(-20, 0, 0))
-	assert.NotNil(t, err, err)
+}
 
+func TestFirstName(t *testing.T) {
 	// Wrong firstname
-	_, err = NewUser("welp.welp@gmail.com", "", "welp", "coucoU12", time.Now().AddDate(-20, 0, 0))
+	_, err := NewUser("welp.welp@gmail.com", "", "welp", "coucoU12", time.Now().AddDate(-20, 0, 0))
 	assert.NotNil(t, err, err)
+}
 
+func TestName(t *testing.T) {
+	// Wrong Name
+	_, err := NewUser("welp.welp@gmail.com", "welp", "", "coucoU12", time.Now().AddDate(-20, 0, 0))
+	assert.NotNil(t, err, err)
+}
+
+func TestEmail(t *testing.T) {
+	// Wrong email
+	_, err := NewUser("@gmail.com", "welp", "welp", "coucoU12", time.Now().AddDate(-20, 0, 0))
+	assert.NotNil(t, err, err)
 	// Wrong email second
 	_, err = NewUser("welp.welp@gmailcom", "welp", "welp", "coucoU12", time.Now().AddDate(-20, 0, 0))
 	assert.NotNil(t, err, err)
+}
 
-	// Wrong Time
-	_, err = NewUser("welp.welp@gmail.com", "welp", "welp", "coucoU12", time.Now().AddDate(20, 0, 0))
-	assert.NotNil(t, err, err)
-
+func TestPassword(t *testing.T) {
 	// Wrong Password no number
-	_, err = NewUser("welp.welp@gmail.com", "welp", "welp", "coucoUazerty", time.Now().AddDate(-20, 0, 0))
+	_, err := NewUser("welp.welp@gmail.com", "welp", "welp", "coucoUazerty", time.Now().AddDate(-20, 0, 0))
 	assert.NotNil(t, err, err)
 
 	// Wrong Password no uppercase
