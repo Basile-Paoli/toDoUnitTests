a35c31c56887635582dd77d51952bdf7959a7388
Basile Paoli <basile.paoli@gmail.com>
2024-10-25 15:36:36 +0200

    dsl j'ai touché à ton code

--- todo/user.go
+++ todo/user.go
@@ -23,58 +23,58 @@ func NewUser(email string, firstname string, name string, password string, birth
 		BirthDate: birthdate,
 	}
 
-	rep, err := user.IsValid()
-	if !rep || err != nil {
+	err := user.ValidateUser()
+	if err != nil {
 		return User{}, err
 	}
 	return user, nil
 }
 
-func (u *User) IsValid() (bool, error) {
+func (u *User) ValidateUser() error {
 	if u.Email == "" || u.Prenom == "" || u.Nom == "" || u.password == "" {
-		return false, fmt.Errorf("Email, Name, Firstame and Password must be filled")
+		return fmt.Errorf("Email, Name, Firstame and Password must be filled")
 	}
 
-	rep, err := u.isEmailValid()
-	if !rep || err != nil {
-		return false, err
+	err := u.validateEmail()
+	if err != nil {
+		return err
 	}
 
-	rep, err = u.isPasswordValid()
-	if !rep || err != nil {
-		return false, err
+	err = u.validatePassword()
+	if err != nil {
+		return err
 	}
 
-	rep, err = u.isBirthdateValid()
-	if !rep || err != nil {
-		return false, err
+	err = u.validateBirthDate()
+	if err != nil {
+		return err
 	}
 
-	return true, nil
+	return nil
 }
 
-func (u *User) isEmailValid() (bool, error) {
+func (u *User) validateEmail() error {
 	var validEmail = regexp.MustCompile(`^[a-z09.-_+]+@[a-z]+\.[a-z]{2,}$`)
 
 	if !validEmail.MatchString(u.Email) {
-		return false, fmt.Errorf("Not a valid email")
+		return fmt.Errorf("Not a valid email")
 	}
-	return true, nil
+	return nil
 }
 
-func (u *User) isBirthdateValid() (bool, error) {
+func (u *User) validateBirthDate() error {
 	yearNow := time.Now().Year()
 	if yearNow-u.BirthDate.Year() <= 13 {
-		return false, fmt.Errorf("You must have 13 Years old to be")
+		return fmt.Errorf("You must have 13 Years old to be")
 	}
-	return true, nil
+	return nil
 }
 
-func (u *User) isPasswordValid() (bool, error) {
+func (u *User) validatePassword() error {
 
 	passLen := len(u.password)
 	if passLen < 8 || passLen > 40 {
-		return false, fmt.Errorf("Password needs to be between 3 and 40 characters")
+		return fmt.Errorf("Password needs to be between 3 and 40 characters")
 	}
 
 	var containsUpperCase = regexp.MustCompile(`[A-Z]`)
@@ -82,8 +82,8 @@ func (u *User) isPasswordValid() (bool, error) {
 	var containsNumber = regexp.MustCompile(`\d`)
 
 	if !containsUpperCase.MatchString(u.password) || !containsLowerCase.MatchString(u.password) || !containsNumber.MatchString(u.password) {
-		return false, fmt.Errorf("Password needs to have at least 1 lower case, 1 uppercase and 1 number")
+		return fmt.Errorf("Password needs to have at least 1 lower case, 1 uppercase and 1 number")
 	}
 
-	return true, nil
+	return nil
 }
